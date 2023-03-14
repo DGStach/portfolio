d1bbad36de9bcee546cc626f9d422bf00f5a6153
dagmara <dagmara.gabriela.stach@gmail.com>
2023-03-14 11:23:34 +0100

    moved scrollReveal.js and tiltAnimation.js up

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import initScrollReveal from "./src/scripts/scrollReveal";
-import initTiltEffect from "./src/scripts/tiltAnimation";
+import initScrollReveal from "./scripts/scrollReveal";
+import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./src/data/scrollRevealConfig";
 
 initScrollReveal(targetElements, defaultProps);
